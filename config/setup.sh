0676e9025e36ac3e48711c8c3e71a22079803746
Samir Patel <samirpatelgx@tmail.com>
2020-02-02 19:43:07 -0800

    Added setup script

---
+++ config/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+if [ "$#" -ge 1 ]; then
+
+  slStatusTemp=0
+  slStatus=0
+
+  sudo apt install tmux
+
+  echo "Initiating git clone of tmux dependent repositories"
+  
+  git clone "https://github.com/tmux-plugins/tpm" "${1}/tmux/plugins/tpm"
+  git clone "https://github.com/tmux-plugins/tmux-resurrect" "${1}/tmux-resurrect"
+  cd "${1}/tmux-resurrect"
+  sh ./resurrect.tmux
+  if [ $? -ne 0 ]; then
+    echo "There was an error when trying to execute the resurrect installation script"
+    exit
+  fi
+
+  for f in $(ls -A ./home/)
+  do
+    ln -s ${PWD}/home/${f} ${1}/${f}
+    slStatusTemp=$?
+    if [ $slStatusTemp -ne 0 ]; then
+      slStatus=${slStatusTemp}
+    fi
+  done
+  if [ $slStatus -ne 0 ]; then
+    echo "There were some unsuccessful operations attempted while trying to create symlinks."
+  else
+    echo "Successfully added all dotfiles to directory ${1}"
+  fi
+else
+  echo "Argument requires a home directory: ./startup.sh <home directory>"
+  echo "Example: startup.sh /home/samirpatelgx"
+fi
+
+
