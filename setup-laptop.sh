0a4bc569589d970c61fc6e8301d88b528bd48c93
Samir Patel <samirpatelgx@gmail.com>
2024-02-22 15:06:06 -0500

    Updated setup-laptop.sh script

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -5,21 +5,23 @@ sudo apt update && sudo apt upgrade -y
 
 # Install Google Chrome
 sudo apt install -y vim curl gimp vlc redis-server redis-tools xclip git
-#curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo apt install ./google-chrome-stable_current_amd64.deb
+curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
 
 # Install Visual Studio Code
 sudo snap install --classic code
 
 # Install OpenJDK 
 
-if [ ! -d "/usr/lib/jvm/jdk-21.0.2"]; then
+if [ ! -d "/usr/lib/jvm/jdk-21.0.2" ]; then
   curl -LO https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz
   tar -xzvf openjdk-21.0.2_linux-x64_bin.tar.gz
-  sudo mv jdk-21.0.2 /usr/lib/jvm
+  sudo mv jdk-21.0.2 /usr/lib/jvm/jdk-21.0.2
   export JAVA_HOME=/usr/lib/jvm/jdk-21.0.2/
   export PATH=$JAVA_HOME/bin:$PATH
   source ~/.bashrc
+  sudo rm openjdk-21.0.2_linux-x64_bin.tar.gz
 fi
 
 sudo snap install gradle --classic
